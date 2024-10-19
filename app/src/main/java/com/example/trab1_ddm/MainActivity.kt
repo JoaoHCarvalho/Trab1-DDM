c6b954ab8e7af95eafc96ca119ea7ee0c7a866eb
João Henrique <143840523+JoaoHCarvalho@users.noreply.github.com>
2024-10-18 23:22:54 -0300

    Update MainActivity.kt

--- app/src/main/java/com/example/trab1_ddm/MainActivity.kt
+++ app/src/main/java/com/example/trab1_ddm/MainActivity.kt
@@ -2,16 +2,17 @@ package com.example.trab1_ddm
 
 import android.os.Bundle
 import android.view.Menu
-import com.google.android.material.snackbar.Snackbar
-import com.google.android.material.navigation.NavigationView
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
+import androidx.drawerlayout.widget.DrawerLayout
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.navigateUp
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
-import androidx.drawerlayout.widget.DrawerLayout
-import androidx.appcompat.app.AppCompatActivity
 import com.example.trab1_ddm.databinding.ActivityMainBinding
+import com.google.android.material.navigation.NavigationView
+import com.google.android.material.snackbar.Snackbar
 
 class MainActivity : AppCompatActivity() {
 
@@ -43,6 +44,9 @@ class MainActivity : AppCompatActivity() {
         )
         setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
+
+
+        val btnmeuso =  findViewById<Button>(R.id.button_alterar_apelido)
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
