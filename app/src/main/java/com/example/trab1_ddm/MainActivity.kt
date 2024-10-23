9ce23bcc4c08da428e9e32885c49f0d4376dfe3a
Lucas Pietro Biasi Rayzer <145388833+LucasRayzer@users.noreply.github.com>
2024-10-23 09:57:29 -0300

    Add RegisterScreen

--- app/src/main/java/com/example/trab1_ddm/MainActivity.kt
+++ app/src/main/java/com/example/trab1_ddm/MainActivity.kt
@@ -68,7 +68,7 @@ class MainActivity : AppCompatActivity() {
         // menu should be considered as top level destinations.
         appBarConfiguration = AppBarConfiguration(
             setOf(
-                R.id.nav_home, R.id.nav_library, R.id.nav_settings
+                R.id.nav_home, R.id.nav_library, R.id.nav_settings, R.id.cadastro
             ), drawerLayout
         )
         setupActionBarWithNavController(navController, appBarConfiguration)
